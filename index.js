5f6539de290db10ed790cca4c5feb2e8bc3c6860
EmmanuelTom <electroetom@gmail.com>
2025-09-06 10:17:08 +0100

    fix path require

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cokkieSession = require('cookie-session');
 const bodyParser = require("body-parser")
 const passport = require('passport');
+const path = require('path');
 require('./models/user');
 require("./services/passport");
 
