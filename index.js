66e2e141cdc9996942983f29eb9b1f1b5a9a5168
EmmanuelTom <electroetom@gmail.com>
2025-09-06 12:26:21 +0100

    fix the path error3

--- index.js
+++ index.js
@@ -28,12 +28,11 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if (true) {
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Serve static assets (main.js, main.css) in production
   app.use(express.static('client/build'))
-  
+
   // Handle React routing, return all requests to React app (Express 5 / path-to-regexp v6 compatible)
   app.get('(.*)', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
