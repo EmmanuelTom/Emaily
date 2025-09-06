4a31098f9595126d21f3f58d748263cb098c85b6
EmmanuelTom <electroetom@gmail.com>
2025-09-06 17:14:04 +0100

    fix the path error4

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
   // Handle React routing, return all requests to React app (Express 5 / path-to-regexp v6 compatible)
-  app.get('(.*)', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('(.*)', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 const PORT = process.env.PORT || 8000;
