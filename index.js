f73fcb68b7323d0fa809b9d3aa00ce3aa16460a1
EmmanuelTom <electroetom@gmail.com>
2025-09-06 11:47:53 +0100

    fix the path error1

--- index.js
+++ index.js
@@ -28,13 +28,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve static assets (main.js, main.css) in production
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+if (true) {
 
+// if (process.env.NODE_ENV === 'production') {
+  // Serve static assets (main.js, main.css) in production
+  app.use(express.static('client/build'))
+  
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
